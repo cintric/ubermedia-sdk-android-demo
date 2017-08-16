b1c380de0b0975b74874d719af826234298532ff
Erwan Lent <erwanlent@gmail.com>
2017-08-16 15:53:43 -0700

    Added SDK init doc image.

--- app/src/main/java/ubermedia/com/headerbiddingdemo/MainActivity.java
+++ app/src/main/java/ubermedia/com/headerbiddingdemo/MainActivity.java
@@ -6,13 +6,13 @@ import android.os.Bundle;
 import ubermedia.com.ubermedia.UberMedia;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
         UberMedia.initializeUberMediaSDK(this, "test-1-api-key", "test-1-secret-key");
-
-        setContentView(R.layout.activity_main);
     }
 }
+
+
